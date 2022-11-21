0c8021229d51f3dffec5e941d35b4d08dd050a9e
issy <issy@issy.dev>
2022-11-21 20:48:05 +0000

    Return meta info from head

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,10 @@
-export default function Head() {
-  <>
-    <meta property='og:title' content='My portfolio'/>
-    <meta property='og:image' content='https://avatars.githubusercontent.com/u/48881813?v=4'/>
-    <meta property='og:description' content='Welcome to my portfolio'/>
-    <meta name='theme-color' content='#ad1457'/>
-  </>
+export default async function Head() {
+  return (
+    <>
+      <meta property="og:title" content="My portfolio"/>
+      <meta property="og:image" content="https://avatars.githubusercontent.com/u/48881813?v=4"/>
+      <meta property="og:description" content="Welcome to my portfolio"/>
+      <meta name="theme-color" content="#ad1457"/>
+    </>
+  );
 }
